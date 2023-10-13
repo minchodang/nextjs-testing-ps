69a37ac9d3a28e65e69bcb3e6424ce9c7a5ac6d2
minchodang <minsu910725@gmail.com>
2023-10-13 09:36:03 +0900

    Feat: band page routing cyp-test 추가

--- base-concert-venue/cypress/integration/routes.test.js
+++ base-concert-venue/cypress/integration/routes.test.js
@@ -3,3 +3,10 @@ it("displays correct heading when navigating to shows route", () => {
   cy.findByRole("button", { name: /shows/i }).click();
   cy.findByRole("heading", { name: /upcoming shows/i }).should("exist");
 });
+it("displays correct heading when navigating to bands route", () => {
+  cy.visit("/");
+  cy.findByRole("button", { name: /bands/i }).click();
+  cy.findByRole("heading", { name: /Our Illustrious Performers/i }).should(
+    "exist"
+  );
+});
