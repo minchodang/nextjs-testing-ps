ee41d44a3c2a296fc1d67d8b52c4ed594b069f64
minsuKang <90169703+minchodang@users.noreply.github.com>
2023-11-23 13:04:40 +0900

    Feat: auth-fail test (#25)

--- base-concert-venue/cypress/e2e/auth/auth-wrapper.cy.js
+++ base-concert-venue/cypress/e2e/auth/auth-wrapper.cy.js
@@ -37,3 +37,44 @@ it("runs auth flow for successful login to protected reservations page", () => {
   // check that sign in button does not exist
   cy.findByRole("button", { name: /sign in/i }).should("not.exist");
 });
+
+it("runs auth flow for protected user page, including failed sign in", () => {
+  cy.task("db:reset").visit("/user");
+
+  cy.findByRole("heading", { name: /sign in to your account/i }).should(
+    "exist"
+  );
+  cy.findByRole("heading", { name: /welcome/i }).should("not.exist");
+
+  cy.findByLabelText(/email address/i)
+    .clear()
+    .type("test@test.test12334");
+  cy.findByLabelText(/password/i)
+    .clear()
+    .type("12345");
+  cy.findByRole("main").within(() => {
+    cy.findByRole("button", { name: /sign in/i }).click();
+  });
+  cy.findByText(/sign in failed/i).should("exist");
+
+  // enter valid sign-in credentials
+  cy.findByLabelText(/email address/i)
+    .clear()
+    .type(Cypress.env("TEST_USER_EMAIL"));
+
+  cy.findByLabelText(/password/i)
+    .clear()
+    .type(Cypress.env("TEST_PASSWORD"));
+
+  // submit the form
+  cy.findByRole("main").within(() => {
+    cy.findByRole("button", { name: /sign in/i }).click();
+  });
+  cy.findByRole("heading", { name: /welcome/i }).should("exist");
+  cy.findByRole("heading", { name: /your tickets/i }).should("exist");
+  cy.findByRole("button", { name: Cypress.env("TEST_USER_EMAIL") }).should(
+    "exist"
+  );
+  cy.findByRole("button", { name: /sign out/i }).should("exist");
+  cy.findByRole("button", { name: /sign in/i }).should("not.exist");
+});
